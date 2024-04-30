5225a514ad61130798d30ce02c9ef82d042991ff
amisha <amisha02@gmail.com>
2024-04-30 12:06:38 +0530

    crud

--- src/Components/Table/View.jsx
+++ src/Components/Table/View.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { Newcontext } from '../Routing/Router'
+import { Button } from 'react-bootstrap'
 
 function View() {
     const [data, setData] = useContext(Newcontext)
@@ -11,10 +12,13 @@ function View() {
         <div className="container mt-5">
             <h3 className='text-center'>VIEW</h3>
             <hr />
-            <div className='mt-5 p-5 bg-light shadow rounded'>
+            <div className='mt-5 p-5 bg-light shadow rounded m-auto'>
                 <h2 >Name: {viewData.name}</h2>
                 <h3>Class: {viewData.class}</h3>
                 <h3>Mark: {viewData.mark}</h3>
+                <Link to={'/'} >
+                    <Button variant="secondary" className='mt-2'> Back</Button>
+                </Link>
             </div>
         </div>
 
